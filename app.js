1140e745dbc4fff3198fc17f616609c909f916c2
yaegashi0314 <a250893h@reitaku.jp>
2026-07-16 12:10:20 +0900

    2

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const plants = [
 // 地図を作成
 map = new maplibregl.Map({
     container: "map",
-    style: "https://api.maptiler.com/maps/hybrid/style.json?key=8eMo5VJPTs3GM9Axz6Dn",
+    style: "https://api.maptiler.com/maps/hybrid-v4/style.json?key=8eMo5VJPTs3GM9Axz6Dn",
     center: [139.95603, 35.83444],
     zoom: 18,
     pitch: 45,
